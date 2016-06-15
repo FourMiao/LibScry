21603283b8da04d5ec48f3a440868148034dcc0c
fourmiao <fourmiao@gmail.com>
2016-06-16 00:04:25 +0800

    Part of KDTree.h Implementation

--- src/kNN/KDTree.h
+++ src/kNN/KDTree.h
@@ -11,6 +11,7 @@
 #include "BoundedPQueue.h"
 #include <stdexcept>
 #include <cmath>
+#include <set>
 
 using namespace std;
 
@@ -40,6 +41,20 @@ public:
 	ElemType KNNValue(const Point<N>& key, size_t k) const;
 	
 private:
+
+	struct ElemNode {
+		Point<N> key;
+		size_t level;
+		ElemType value;
+		ElemNode *lson;
+		ElemNode *rson;
+	};
+	size_t numOfElems;
+	ElemNode *root;
+
+	ElemType& findNode(const Point<N>& pt) const;
+	void findKNNValue(const Point<N>& key, const BoundedPQueue<ElemNode*>& bpq) const;
+	ElemType getMostFrequentValue(const BoundedPQueue<ElemNode*>& bpq) const;
 };
 
 template <size_t N, typename ElemType>
@@ -54,7 +69,82 @@ KDTree<N, ElemType>::~KDTree() {
 
 template <size_t N, typename ElemType>
 size_t KDTree<N, ElemType>::dimension() const {
-	return 0;
+	return N;
+}
+
+template <size_t N,  typename ElemType>
+size_t KDTree<N, ElemType>::size() const {
+	return numOfElems;
+}
+
+template <size_t N, typename ElemType>
+void KDTree<N, ElemType>::empty() const {
+	return numOfElems == 0;
+}
+
+template <size_t N, typename ElemType>
+bool KDTree<N, ElemType>::contains(const Point<N>& pt) const {
+	return findNode(pt) != NULL;
+}
+
+template <size_t N, typename ElemType>
+ElemType& KDTree<N, ElemType>::findNode(const Point<N>& pt) const {
+	for (ElemNode *currNode = root, size_t level = 0; currNode != NULL; ++level) {
+		size_t keyIndex = level % N;
+		if (currNode->key == pt) {
+			return currNode->vale;
+		}
+		if (pt[keyIndex] < currNode->key[keyIndex]) {
+			currNode = currNode->lson;
+		} else {
+			currNode = currNode->rson;
+		}
+	}
+	return NULL;
+}
+
+template <size_t N, typename ElemType>
+ElemType& KDTree<N, ElemType>::operator[](const Point<N>& pt) {
+	return findNode(pt);
+}
+
+template <size_t N, typename ElemType>
+ElemType KDTree<N, ElemType>::KNNValue(const Point<N>& pt, size_t k) const {
+	BoundedPQueue<ElemNode*> bpq(k);
+
+}
+
+template <size_t N, typename ElemType>
+void KDTree<N, ElemType>::findKNNValue(const Point<N>& key, const BoundedPQueue<ElemNode*>& bpq) const {
+	for (ElemNode* currNode = root, size_t level = 0; currNode != NULL; ++level) {
+		size_t keyIndex = level % N;
+		bpq.enqueue(currNode->value, Distance(currNode->key, key);
+
+		if (key[keyIndex] < currNode->key[keyIndex]) {
+				
+		}
+	}
+}
+
+template <size_t N, typename ElemType>
+ElemType KDTree<N, ElemType>::getMostFrequentValue(const BoundedPQueue<ElemNode*>& bpq) const {
+	multiset<ElemType> valueCount;
+
+	while (!bpq.empty()) {
+		valueCount.insert((bpq.dequeueMin())->value);
+	}
+
+	size_t mostFrequentCount = 0;
+	ElemType mostFrequentValue = NULL;
+
+	for (typename multiset<ElemType>::iterator it = valueCount.begin(); it != valueCount.end(); ++it) {
+		if (valueCount.count(*it) > mostFrequentCount) {
+			mostFrequentCount = valueCount.count(*it);
+			getMostFrequentValue = *it;
+		}
+	}
+
+	return mostFrequentValue;
 }
 
 #endif //KDTREE_INCLUDED
